b2003cbdccb9729e031cd1a45020b78619a84e7d
VakaPrasanna <20BQ1A05O6@vvit.net>
2023-08-03 02:04:44 +0530

    Name Nav

--- mynewsite/main/views.py
+++ mynewsite/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
-def index(response, id):
-    return HttpResponse("<h1>%s</h1>" % id)
+def index(response, name):
+    ls= ToDoList.objects.get(name=name)
+    return HttpResponse("<h1>%s</h1>" % ls.name)
