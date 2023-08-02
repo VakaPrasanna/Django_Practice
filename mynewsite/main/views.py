0886fb1c9d37dc7c418fcdf0b70a1d8a107e0a8b
VakaPrasanna <20BQ1A05O6@vvit.net>
2023-08-03 01:54:06 +0530

    Python shell and Number Nav

--- mynewsite/main/views.py
+++ mynewsite/main/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>GuruDeva</h1>")
-def v1(response):
-    return HttpResponse("<h1>View 1!</h1>")
+def index(response, id):
+    return HttpResponse("<h1>%s</h1>" % id)
