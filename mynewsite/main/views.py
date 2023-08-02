bed61830ed382bffb8ca0c5b163405daff77482c
VakaPrasanna <20BQ1A05O6@vvit.net>
2023-08-03 02:12:33 +0530

    ToDoList Implemented

--- mynewsite/main/views.py
+++ mynewsite/main/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 def index(response, name):
-    ls= ToDoList.objects.get(name=name)
-    return HttpResponse("<h1>%s</h1>" % ls.name)
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
